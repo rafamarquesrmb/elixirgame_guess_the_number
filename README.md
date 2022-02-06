2f354159675a62c5d5c0a0062df242bae1001039
rafamarquesrmb <rafa.rmb@gmail.com>
2022-02-06 03:07:40 -0300

    Projeto principal finalizado

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
-# Guess
+[![author](https://img.shields.io/badge/author-rafamarquesrmb-blue.svg)](https://github.com/rafamarquesrmb) [![DotNET6](https://img.shields.io/badge/Elixir-1.13-purple.svg)](https://elixir-lang.org/) [![License-Unlicense](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
 
-**TODO: Add description**
+# Guess the Number
 
-## Installation
+<sub>Developed with [Elixir](https://elixir-lang.org/) by [Rafael Marques](https://github.com/rafamarquesrmb)</sub>
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `guess` to your list of dependencies in `mix.exs`:
+**Guess the Number** is a simple console game where you have 3 difficulty levels. You will play against the computer, and you must match the number the computer has chosen as quickly as possible! If you make a mistake, the computer will give you a hint if you are above or below the number informed.
 
-```elixir
-def deps do
-  [
-    {:guess, "~> 0.1.0"}
-  ]
-end
-```
+- Level 1 has a range between 1 and 10.
+- Level 2 has a range between 1 and 100.
+- Level 3 has a range between 1 and 1000.
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/guess>.
+## About
 
+This application runs on the console. It was built with **Elixir**, a powerful and modern functional programming language.
+
+It was inspired by the tutorial by "_David Alencar_", a Brazilian Developer, founder of [Elly Academy](https://github.com/ellyacademy). It has a [YouTube channel](https://www.youtube.com/channel/UCED1QpeMYP4apdCVGjBqhCg) and a [playlist](https://www.youtube.com/watch?v=GXlyLSVDkhI&list=PLydk1OOOmzo8VBeU334j4R4WvSByRNpXR) with an introduction to Elixir, where this app was built.
--- lib/guess.ex
+++ lib/guess.ex
@@ -1,18 +1,92 @@
 defmodule Guess do
-  @moduledoc """
-  Documentation for `Guess`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  def start(_, _) do
+    run()
+    {:ok, self()}
+  end
 
-  ## Examples
+  def run() do
+    IO.puts("Let's play Guess the Number")
 
-      iex> Guess.hello()
-      :world
+    IO.gets("Pick a difficult level(1,2 or 3): ")
+    |> parse_input()
+    |> pickup_number()
+    |> play()
+  end
 
-  """
-  def hello do
-    :world
+  def play(picked_num) do
+    IO.gets("I have my number. What is your guess?")
+    |> parse_input()
+    |> guess(picked_num, 1)
+  end
+
+  def guess(usr_guess, picked_num, count) when usr_guess > picked_num do
+    IO.gets("Too high. Guess again:\n")
+    |> parse_input()
+    |> guess(picked_num, count + 1)
+  end
+
+  def guess(usr_guess, picked_num, count) when usr_guess < picked_num do
+    IO.gets("Too low. Guess again: \n")
+    |> parse_input()
+    |> guess(picked_num, count + 1)
+  end
+
+  def guess(_usr_guess, _picked_num, count) do
+    IO.puts("\n\nYou got it in #{count} guesses")
+    show_score(count)
+  end
+
+  def show_score(guesses) when guesses > 6, do: IO.puts("Better luck next time")
+
+  def show_score(guesses) do
+    {_, msg} =
+      %{
+        (1..1) => "You're a mind rider!",
+        (2..4) => "Most impressive",
+        (3..6) => "You can do better than that"
+      }
+      |> Enum.find(fn {range, _} ->
+        Enum.member?(range, guesses)
+      end)
+
+    IO.puts(msg)
+  end
+
+  def parse_input(:error) do
+    IO.puts("Invalid input!!!")
+    run()
+  end
+
+  def parse_input({num, _}), do: num
+
+  def parse_input(data) do
+    data
+    |> Integer.parse()
+    |> parse_input()
+  end
+
+  def pickup_number(level) do
+    level
+    |> get_range()
+    |> Enum.random()
+  end
+
+  def get_range(level) do
+    case level do
+      1 ->
+        1..10
+
+      2 ->
+        1..100
+
+      3 ->
+        1..1000
+
+      _ ->
+        IO.puts("Invalid level!!!")
+        run()
+    end
   end
 end
